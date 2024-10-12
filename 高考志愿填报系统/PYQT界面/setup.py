77566d0c2025d19a824520c92da65732dab78ee2
username <useremail@qq.com>
2024-10-12 22:54:24 +0800

    1

--- 高考志愿填报系统/PYQT界面/setup.py
+++
@@ -1,8 +0,0 @@
-# PYD打包【可以打包但是缺少C++环境】
-# pip install setuptools
-from setuptools import setup
-from Cython.Build import cythonize
-setup(
-    name='tmp_debug',
-    ext_modules=cythonize("main.py"),
-)
--- 高考志愿填报系统/程序打包/程序打包.py
+++
@@ -1,7 +0,0 @@
-# # 【安装pyinstaller库】
-# pip install pyinstaller
-# # 【打包单个文件】
-# pyinstaller -F C:\Users\13480\gitee\trade\高考志愿填报系统\软件强制更新\版本验证.py
-
-#【有一些python库不那么兼容pyinstaller的打包方式，可以换成用nuitka打包】
-# nuitka
